1f41c1db0e010c94ef05ceb43783791ecdc51a8e
yourname <your email>
2018-11-21 18:21:48 +0800

    Initial commit

---
+++ src/main/java/com/mall/hello/spring/boot/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.mall.hello.spring.boot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String getWechatOpenid(){
+        return "index";
+    }
+}
